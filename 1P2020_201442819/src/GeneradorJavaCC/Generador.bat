09bfbb4a060154431982ccb629b21fa177d7b719
JerduarV <201442819@ingenieria.usac.edu.gt>
2020-02-18 01:36:29 -0600

    Se agregó generador de JavaCC y plantilla

---
+++ 1P2020_201442819/src/GeneradorJavaCC/Generador.bat
@@ -0,0 +1,2 @@
+javacc GramaticaJCC.jj
+pause
--- 1P2020_201442819/src/pkg1p2020_201442819/Main.java
+++ 1P2020_201442819/src/pkg1p2020_201442819/Main.java
@@ -18,7 +18,6 @@ public class Main {
      */
     public static void main(String[] args) {
         new VentanaEditor();
-        System.out.println("HOLA MUNDO");
     }
     
 }
